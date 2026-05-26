0261fb7b75838be031cb1661ceea7a00ba24a1f9
AjloriaCodex <ajloria123@gmail.com>
2026-05-26 20:56:37 +0800

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/illust/', // <--- ADD THIS LINE
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
